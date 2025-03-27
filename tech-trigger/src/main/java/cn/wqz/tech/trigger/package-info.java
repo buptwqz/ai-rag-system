5c42c423165794c99bd2145ccf1266c194631871
buptwqz <879680229@qq.com>
2025-03-27 20:26:34 +0800

    创建项目

---
+++ tech-trigger/src/main/java/cn/wqz/tech/trigger/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @Author: Qizheng Wang
+ * @Email: qizheng.wang@foxmail.com
+ * @Date: 2025/3/27 下午8:08
+ * @GitHub: https://github.com/buptwqz
+ * @Description:
+ **/
+package cn.wqz.tech.trigger;
